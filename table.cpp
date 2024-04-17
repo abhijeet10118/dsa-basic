1ee3a3bb05b2cb83556cf79486de5bb8b1b721d9
abhijeet10118 <167314334+abhijeet10118@users.noreply.github.com>
2024-04-17 23:06:43 +0530

    Add files via upload

---
+++ table.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+   
+   int a ;
+   cin >> a ;
+   int i = 1 ;
+   while (i <= 10) {
+    cout << a <<" * "<< i <<" = "<< a*i << endl ;
+    i=i+1 ;
+   }
+     
+}     
