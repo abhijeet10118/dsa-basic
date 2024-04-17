ee7cafa903aa928d2a783294409ab764b20a4a67
abhijeet10118 <167314334+abhijeet10118@users.noreply.github.com>
2024-04-17 22:31:55 +0530

    Add files via upload

---
+++ while loop print till n.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    cout << " give the number till where u want to print"<< endl ;
+    int a ;
+    cin >> a ;
+    int i = 1 ;
+
+    while (i<=a){
+        cout << i << endl ;
+        i=i+1 ;
+    }
+     
+
+}
